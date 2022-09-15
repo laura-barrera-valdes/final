91976eb1366e4bc94584bee9a56a35f9e60499ed
Laura Barrera Valdés <laurabv99@gmail.com>
2022-09-15 10:58:42 -0500

    ajuste la distribucion del post

--- public/src/components/Post/Post.js
+++ public/src/components/Post/Post.js
@@ -24,38 +24,42 @@ class Post extends HTMLElement {
         <link rel="stylesheet" href="/src/components/Post/styles.css">
         <section class="cuadro">
 
-            <div class="div1">
-            <image class="profph" src="${this.profph}"></image>
-            <h1 class="nickname">${this.nickname}</h1>
-            <image class="settingsicon" src="./assets/icons/Settings-icon.png"></image>
+            <div class="vertical">
+            
+                <div class="div1">
+                <image class="profph" src="${this.profph}"></image>
+                <h1 class="nickname">${this.nickname}</h1>
+                <image class="settingsicon" src="./assets/icons/Settings-icon.png"></image>
+                </div>
+
+                <div class="div2">
+                <image class="thepost" src="${this.post}"></image>
+                </div>
+
+                <div class="div3">
+                <image class="likeicon" src="./assets/icons/Like-icon-off.png"></image>
+                <image class="commenticon" src="./assets/icons/Comment-icon.png"></image>
+                <image class="shareicon" src="./assets/icons/Share-icon.png"></image>
+                <image class="saveicon" src="./assets/icons/Save-icon.png"></image>
+                </div>
+
+                <div class="div4">
+                <h2 class="likes">${this.likes}</h2>
+                <h2 class="views">${this.views}</h2>
+                </div>
+
+                <div class="div5">
+                <h2 class="nicknamedescr">${this.nickname}</h2>
+                <h2 class="descr">${this.descr}</h2>
+                </div>
+
+                <div class="div6">
+                <h3 class="comments">${this.comments}</h3>
+                <h4 class="date">${this.date}</h4>
+                </div>
+                
             </div>
 
-            <div class="div2">
-            <image class="thepost" src="${this.post}"></image>
-            </div>
-
-            <div class="div3">
-            <image class="likeicon" src="./assets/icons/Like-icon-off.png"></image>
-            <image class="commenticon" src="./assets/icons/Comment-icon.png"></image>
-            <image class="shareicon" src="./assets/icons/Share-icon.png"></image>
-            <image class="saveicon" src="./assets/icons/Save-icon.png"></image>
-            </div>
-
-            <div class="div4">
-            <h2 class="likes">${this.likes}</h2>
-            <h2 class="views">${this.views}</h2>
-            </div>
-
-            <div class="div5">
-            <h2 class="nicknamedescr">${this.nickname}</h2>
-            <h2 class="descr">${this.descr}</h2>
-            </div>
-
-            <div class="div6">
-            <h3 class="comments">${this.comments}</h3>
-            <h4 class="date">${this.date}</h4>
-            </div>
-        
         </section>` 
     }
 }
